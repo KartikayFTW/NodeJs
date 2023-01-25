86e7e3fc720f86a68140f4435bcd9705e8df9474
Kartikay <kartikaydwivedi55@gmail.com>
2023-01-25 23:09:58 +0530

    feat: "package json added using npm init"

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const colors = require("colors");
+
 console.log("Hello World");
+console.log("Hello World".red);
+console.log("Hello World".green);
+console.log("Hello World".bgBlue);
+console.log("Hello World".bgRed);

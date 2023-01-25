f74913ae45cc03ae7c27dd58e7a470169c23d8bd
Kartikay <kartikaydwivedi55@gmail.com>
2023-01-25 23:32:46 +0530

    feat: a simple api created

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+  { name: "kumar", email: "jj@dd.com", contact: "9898" },
+  { name: "sam", email: "jj@ddone.com" },
+  { name: "peter", email: "ff@dd.com" },
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const http = require("http");
+const data = require("./data");
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    // res.write(JSON.stringify({ name: "kumar", email: "jj@dd.com" }));
+    res.write(JSON.stringify(data));
+    res.end();
+  })
+  .listen(4500);

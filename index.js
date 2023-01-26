3ea262dd9b7313f713083b9c7656c874e1b62360
Kartikay <kartikaydwivedi55@gmail.com>
2023-01-26 23:02:35 +0530

    feat: async example

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+let a = 20;
+let b = 0;
+// setTimeout(() => {
+//   b = 10;
+// }, 1000);
+let waitingData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve((b = 30));
+  }, 1000);
+});
+waitingData.then(() => {
+  console.log(a + b);
+});
+// console.log(a + b);

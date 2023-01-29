68ea8c3960f55cb0d2c1539a4e501375903bcd6d
Kartikay <kartikaydwivedi55@gmail.com>
2023-01-29 22:58:11 +0530

    feat: static html pages added

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const publicPath = path.join(__dirname, "public");
+
+app.use(express.static(publicPath));
+app.listen(3001, console.log("server running"));
